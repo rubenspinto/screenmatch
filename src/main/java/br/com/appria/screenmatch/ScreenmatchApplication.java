53944b8915ab9bd7372a8ebf178ec9081df03212
rubenspinto <rubensms825@gmail.com>
2024-06-11 02:34:01 -0300

    Modelando dados da série

--- src/main/java/br/com/appria/screenmatch/ScreenmatchApplication.java
+++ src/main/java/br/com/appria/screenmatch/ScreenmatchApplication.java
@@ -1,5 +1,8 @@
 package br.com.appria.screenmatch;
 
+import br.com.appria.screenmatch.model.DadosSerie;
+import br.com.appria.screenmatch.service.ConsumoAPI;
+import br.com.appria.screenmatch.service.ConverteDados;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -13,6 +16,14 @@ public class ScreenmatchApplication implements CommandLineRunner {
 
 	@Override
 	public void run(String... args) throws Exception {
+
+		var consumoApi = new ConsumoAPI();
+		var json = consumoApi.obterDados("https://www.omdbapi.com/?t=gilmore+girls&apikey=e44aa044");
+		System.out.println(json);
+
+		var conversor = new ConverteDados();
+		var dados = conversor.obterDados(json, DadosSerie.class);
+		System.out.println(dados);
 
 	}
 
